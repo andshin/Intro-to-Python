ce3a5507733e2c38e556c4f428b995fd8d6a38fe
andshin <mr.shin@outlook.com>
2017-04-13 13:57:15 +0100

    Update largest odd number from list of 10 numbers.py

--- Chapter 2/largest odd number from list of 10 numbers.py
+++ Chapter 2/largest odd number from list of 10 numbers.py
@@ -1,5 +1,3 @@
-#book page 20
-
 #write a program that asks the user to input 10 integeres, 
 #and the nprints the largest odd number that was entered.
 #if no odd nubmer entered it should print a msg to that effect.
@@ -8,61 +6,61 @@ x = 0
 oddcheck = 0
 
 n1 = int(input("Enter the first nubmer: "))
-if n1 != 0 and n1 > x:
+if n1 % 2 != 0 and n1 > x:
     x = n1
 else:
     oddcheck += 1
     
 n2 = int(input("Enter the second nubmer: "))
-if n2 != 0 and n2 > x:
+if n2 % 2 != 0 and n2 > x:
     x = n2
 else:
     oddcheck += 1
     
 n3 = int(input("Enter the third nubmer: "))
-if n3 != 0 and n3 > x:
+if n3 % 2 != 0 and n3 > x:
     x = n3
 else:
     oddcheck += 1
     
 n4 = int(input("Enter the forth nubmer: "))
-if n4 != 0 and n4 > x:
+if n4 % 2 != 0 and n4 > x:
     x = n4
 else:
     oddcheck += 1
 
 n5 = int(input("Enter the fifth nubmer: "))
-if n5 != 0 and n5 > x:
+if n5 % 2 != 0 and n5 > x:
     x = n5
 else:
     oddcheck += 1
 
 n6 = int(input("Enter the sixth nubmer: "))
-if n6 != 0 and n6 > x:
+if n6 % 2 != 0 and n6 > x:
     x = n6
 else:
     oddcheck += 1
 
 n7 = int(input("Enter the seventh nubmer: "))
-if n7 != 0 and n7 > x:
+if n7 % 2 != 0 and n7 > x:
     x = n7
 else:
     oddcheck += 1
 
 n8 = int(input("Enter the eigth nubmer: "))
-if n8 != 0 and n8 > x:
+if n8 % 2 != 0 and n8 > x:
     x = n8
 else:
     oddcheck += 1
 
 n9 = int(input("Enter the ninth nubmer: "))
-if n9 != 0 and n9 > x:
+if n9 % 2 != 0 and n9 > x:
     x = n9
 else:
     oddcheck += 1
 
 n10 = int(input("Enter the tenth and last nubmer: "))
-if n10 != 0 and n10 > x:
+if n10 % 2 != 0 and n10 > x:
     x = n10
 else:
     oddcheck += 1
